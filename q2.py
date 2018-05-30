8d4abc2a3f43b4add3b36dcec4de1f23dc44afd9
ManOfTool <a101013322@gmail.com>
2018-05-30 12:57:32 +0800

    Rename file

---
+++ q2.py
@@ -0,0 +1,19 @@
+from Cubicspline import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+n = int(input('Value of n = '))
+
+x, fx, h = enterNodes(n)
+
+a = fx
+b, c, d = coeBCD(n, a, h)
+
+retDisplay(n, x, a, b, c, d)
+saveFigure(n, x, a, b, c, d, 'q1_2.png')
+
+xx = float(input('Predict the position of the car when x = '))
+l_most = getInterval(xx, n, x)
+value = predictValue(xx, x[l_most], a[l_most], b[l_most], c[l_most], d[l_most])
+print('When x = {}, S(x) = {}'.format(xx, value))
