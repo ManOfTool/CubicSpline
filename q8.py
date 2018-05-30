65746fec1f8bcbbcaefa239d989abe1ebb2a6d32
ManOfTool <a101013322@gmail.com>
2018-05-30 15:00:40 +0800

    Complete q8

---
+++ q8.py
@@ -0,0 +1,46 @@
+from Cubicspline import *
+import math
+
+n = 81
+
+t = []
+x = []
+y = []
+h = []
+
+for i in range(0, n+1):
+    t.append(round(0 + (2 * np.pi / n) * i, 6))
+
+x = np.cos(t)
+y = np.sin(t)
+
+for i in range(0, n+1):
+    x[i] = round(x[i], 6)
+    y[i] = round(y[i], 6)
+
+for j in range(0, n):
+    h.append(t[j+1] - t[j])
+
+a1 = x
+b1, c1, d1 = coeBCD(n, a1, h)
+
+a2 = y
+b2, c2, d2 = coeBCD(n, a2, h)
+
+print('Coefficient of Sx(t):')
+retDisplay(n, t, a1, b1, c1, d1)
+
+print('')
+print('Coefficient of Sy(t):')
+retDisplay(n, t, a2, b2, c2, d2)
+
+for i in range(0, n):
+    tt = np.arange(t[i], t[i+1], 0.0002)
+
+    tmp = tt - t[i]
+    xx = a1[i] + b1[i] * tmp + c1[i] * pow(tmp, 2) + d1[i] * pow(tmp, 3)
+    yy = a2[i] + b2[i] * tmp + c2[i] * pow(tmp, 2) + d2[i] * pow(tmp, 3)
+
+    plt.plot(xx, yy)
+
+plt.savefig('q8.png')
